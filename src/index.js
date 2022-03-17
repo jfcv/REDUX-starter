97c54e6155828fb5a15bc5cd9c132a708a0bfc86
Juan Cuadros <juan.cuadros@pm.me>
2022-03-16 22:56:10 -0500

    Implement the toastify middleware.

--- src/index.js
+++ src/index.js
@@ -16,29 +16,11 @@ const unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch((dispatch, getState) => {
-  // call an API
-  // promise resolved -> dispatch()
   dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
   console.log(getState());
-  // promise rejected -> dispatch()
 });
 
-// store.dispatch(userAdded({ name: "User 1" }));
-// store.dispatch(userAdded({ name: "User 2" }));
-
-// store.dispatch(projectAdded({ name: "Project 1" }));
-
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-// store.dispatch(bugResolved({ id: 1 }));
-
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-// store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
-
-// console.log("get bugs by user!");
-// const bugsUser1 = getBugsByUser(1)(store.getState());
-// console.log(bugsUser1);
-
-// console.log("unresolved bugs!");
-// console.log(getUnresolvedBugs(store.getState()));
+store.dispatch({
+  type: "error",
+  payload: { message: "An error ocurred." },
+});
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
+import toast from "./middleware/toast";
 
 const config = () => {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger],
+    middleware: [...getDefaultMiddleware(), logger, toast],
   });
 };
 
