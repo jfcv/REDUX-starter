271acfd47300ba22b38d6616a1c089910a5e6b0e
Juan Cuadros <juan.cuadros@pm.me>
2022-03-16 22:48:45 -0500

    Define getDefaultMiddleware() from @reduxjs/toolkit to implement redux thunk at the middlewares in the configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
-import func from "./middleware/func";
 
 const config = () => {
-  return configureStore({ reducer, middleware: [logger, func] });
+  return configureStore({
+    reducer,
+    middleware: [...getDefaultMiddleware(), logger],
+  });
 };
 
 export default config;
