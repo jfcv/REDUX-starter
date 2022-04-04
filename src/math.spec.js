08b14d96896b9a924dbd73cee9be6bff553a4c19
Juan Cuadros <juan.cuadros@pm.me>
2022-04-04 10:59:39 -0500

    Implement first unit test suite [isEven].

---
+++ src/math.spec.js
@@ -0,0 +1,16 @@
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  it("Should return true if given an even number", () => {
+    /**
+     * function under test (SUT: system under test)
+     */
+    const result = isEven(2);
+    expect(result).toEqual(true);
+  });
+
+  it("Should return false if given an odd number", () => {
+    const result = isEven(1);
+    expect(result).toEqual(false);
+  });
+});
